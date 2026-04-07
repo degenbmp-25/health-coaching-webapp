60e563f6a056932713eb13ccfe2741ebd4fb8340
Beastmodeproductions <degenbmp-25@users.noreply.github.com>
2026-04-07 10:11:19 -0400

    fix: move sidebar outside flex container for proper mobile centering

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -17,23 +17,25 @@ export default async function DashboardLayout({
   return (
     <div className="flex min-h-screen flex-col space-y-6 px-4">
       <Navbar />
-      {/* Content area: full width on mobile, sidebar overlaps on desktop */}
-      <div className="relative flex flex-1">
-        {/* Sidebar: absolute on desktop, hidden on mobile */}
-        <aside className="absolute left-0 top-0 hidden lg:block w-[200px] h-full">
-          <DashboardNav items={dashboardLinks.data} />
-        </aside>
-        {/* Main content: full width on mobile, indented on desktop */}
-        <main className="flex w-full flex-1 flex-col relative">
-          {/* Mobile hamburger nav */}
-          <div className="fixed top-20 left-4 z-50 lg:hidden">
-            <MobileNav items={mobileLinks} />
-          </div>
-          <div className="lg:pl-[216px] px-4">
-            {children}
-          </div>
-        </main>
-      </div>
+
+      {/* DESKTOP ONLY: Fixed sidebar that overlays content */}
+      <aside className="fixed left-0 top-16 bottom-0 w-[200px] z-30 hidden lg:block">
+        <DashboardNav items={dashboardLinks.data} />
+      </aside>
+
+      {/* Main content: full width on mobile, with left indent on desktop */}
+      <main className="flex w-full flex-1 flex-col relative">
+        {/* Mobile hamburger nav */}
+        <div className="fixed top-20 left-4 z-50 lg:hidden">
+          <MobileNav items={mobileLinks} />
+        </div>
+
+        {/* Content: px-4 on mobile (16px padding), lg:pl-[216px] on desktop */}
+        <div className="px-4 lg:pl-[216px] w-full">
+          {children}
+        </div>
+      </main>
+
       <Footer />
     </div>
   )
